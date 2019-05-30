a603e257a29cf3aa7e4cb3ac0be88dee5d60255b
Crystal-Keng <c980509@gmail.com>
2019-05-30 16:00:59 +0800

    hw2

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edNtd = findViewById(R.id.ntd);
+        edNtd = findViewById(R.id.ntd); 
     }
 
     public void go(View view) {
